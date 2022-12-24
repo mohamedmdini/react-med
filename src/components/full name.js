1a03901ada1cc2c84fee189a67fba1a21d4ed268
mohamedmdini <mdinimohamed14@gmail.com>
2022-12-24 16:28:54 +0100

    first commit

---
+++ src/components/full name.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function ImportExport2() {
+return (
+        <h1>
+        my name is mohamed mdini 
+        </h1>
+);
+
+}
+
+export default ImportExport2; 
---
+++ src/components/upload image.js
@@ -0,0 +1,18 @@
+import React from "react";
+import keep2 from '../images/keep1.jpg';
+function Upload() {
+    return (
+        <div>
+
+        <img src={keep2} alt=""/>
+        
+        </div>
+
+
+
+    );
+
+}
+
+export default Upload; 
+
